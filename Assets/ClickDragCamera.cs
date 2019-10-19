f7e459a5e2dc92432de652aba8e1c8e0007e378d
Nathaniel Bott <natbott97@gmail.com>
2019-10-19 23:08:06 +1100

    Added Unity files

---
+++ Assets/ClickDragCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDragCamera : MonoBehaviour {
+    public Transform target;
+    public float rotateSpeed;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    public float yMin, yMax;
+
+    public bool invertY;
+
+    private Vector3 offset;
+
+    void Start() {
+        offset = transform.position - target.transform.position;
+    }
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            x += Input.GetAxis("Mouse X") * rotateSpeed;
+
+            if (invertY) {
+                y += Input.GetAxis("Mouse Y") * rotateSpeed;
+            } else {
+                y -= Input.GetAxis("Mouse Y") * rotateSpeed;
+            }
+        }
+
+        //y = Mathf.Clamp(y, yMin, yMax);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        transform.rotation = rotation;
+
+        transform.position = Vector3.Lerp(transform.position, rotation * offset + target.transform.position, 0.5f);
+        transform.LookAt(target);
+
+        offset.z += Input.mouseScrollDelta.y;
+
+        if (Input.GetKeyDown(KeyCode.Y)) {
+            invertY = !invertY;
+        }
+    }
+}
---
+++ Assets/ModelSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSwitcher : MonoBehaviour {
+
+    public List<GameObject> models = new List<GameObject>();
+
+    public int currentModel;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            currentModel -= 1;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            currentModel += 1;
+        }
+
+        if (currentModel > (models.Count-1)) {
+            currentModel = 0;
+        } else if (currentModel < 0) {
+            currentModel = (models.Count-1);
+        }
+
+        foreach (GameObject x in models) {
+            x.SetActive(false);
+        }
+
+        models[currentModel].SetActive(true);
+    }
+
+    public void ChangeModel(int nextModel) {
+        currentModel = nextModel;
+    }
+}
